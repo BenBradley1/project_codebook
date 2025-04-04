205441211676c479adc3dc3cceada8bd5661cf02
Ben Bradley <vlqc7666@leeds.ac.uk>
2025-04-04 09:29:47 +0100

    test file

---
+++ scripts/test.py
@@ -0,0 +1,12 @@
+def Helloworld(text:str):
+    """prints hello world
+    and returns the text
+
+    Args:
+        text (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    print(f"Hello world {text}")
+    return text
